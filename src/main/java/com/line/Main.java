af0350a9c8cf5e1eec81916f119efbfaa881ecb4
JO YEJI <whdpwl2@gmail.com>
2022-10-12 23:29:27 +0900

    sql파일 작성

--- src/main/java/com/line/Main.java
+++ src/main/java/com/line/Main.java
@@ -3,6 +3,9 @@ package com.line;
 import com.line.domain.Hospital;
 import com.line.parser.HospitalParser;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +14,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         LineReader<Hospital> hospitalLineReader = new LineReader<>(new HospitalParser());
         String filename = "../data/서울시병의원위치11.csv";
-        List<Hospital> hospitals = hospitalLineReader.readLines(filename);
 
+        List<Hospital> hospitals = hospitalLineReader.readLines(filename);
+        List<String> lines = new ArrayList<>();
         for (Hospital hospital : hospitals) {
             String line = hospital.getAllForSql();
-            System.out.println(line);
+            lines.add(line);
         }
+
+
+        File file = new File("./src/main/java/com/line/seoul_hospital_insert.sql");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        for (String line : lines) {
+            writer.write(line);
+        }
+        writer.close();
     }
 }
