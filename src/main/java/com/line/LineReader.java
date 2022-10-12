a9c2eb6bcb2ba3c098403cc1c41cd6aa33395660
JO YEJI <whdpwl2@gmail.com>
2022-10-12 15:22:41 +0900

    파일 생성하고 라인 읽어오는 클래스

--- src/main/java/com/line/LineReader.java
+++ src/main/java/com/line/LineReader.java
@@ -1,2 +1,30 @@
-package com.line;public class LineReader {
+package com.line;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LineReader {
+
+    public List<String> readLines(String filename) throws IOException {
+        List<String> result = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String str;
+        while ((str = br.readLine()) != null) {
+            result.add(str);
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        LineReader lr = new LineReader();
+        String filename = "../data/서울시 병의원 위치 정보.csv";
+        List<String> hospitals = lr.readLines(filename);
+        System.out.println(hospitals.size());
+
+    }
 }
