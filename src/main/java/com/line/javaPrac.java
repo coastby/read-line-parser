20aa3098bc9978a3cd282dc0f190d8982b8375d5
JO YEJI <whdpwl2@gmail.com>
2022-10-12 22:58:17 +0900

    sql file create

--- src/main/java/com/line/javaPrac.java
+++ src/main/java/com/line/javaPrac.java
@@ -1,7 +1,12 @@
 package com.line;
 
-public class javaPrac {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class javaPrac {
+    public static void main(String[] args) throws IOException {
+        File file = new File("./src/main/java/com/line/seoul_hospital_insert.sql");
+        file.createNewFile();
     }
 }
