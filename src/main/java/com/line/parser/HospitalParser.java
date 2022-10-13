4cef41b76e01498b868ec5970d7edc79264cf8d9
JO YEJI <whdpwl2@gmail.com>
2022-10-13 13:26:53 +0900

    tese code address 추가

--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -6,6 +6,8 @@ public class HospitalParser implements Parser<Hospital> {
     //csv파일에서 한 줄씩 받아서 객체로 만들어서 반환한다
     @Override
     public Hospital parse(String str) {
+        //"(쌍따옴표)가 있는 파일이라면 아래 코드 사용, 현재 csv 파일에서는 없어서 사용 안 함
+//        str = str.replace("\"","");
         //csv 파일을 받기 때문에
         String[] splitted = str.split(",");
         //id, address, category, emergencyRoom, name, subdivision
