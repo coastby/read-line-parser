60e0ee1ecf8999708811b0c7d79a0b4380ad95f2
JO YEJI <whdpwl2@gmail.com>
2022-10-13 23:47:32 +0900

    setSubdivision() Hospital에서 HospitalParser 클래스로 옮김 : 혹시 세부과를 같이 등록할 수도 있어서

--- src/main/java/com/line/domain/Hospital.java
+++ src/main/java/com/line/domain/Hospital.java
@@ -10,16 +10,14 @@ public class Hospital {
     private String name;
     private String subdivision;
 
-    public Hospital(String id, String address, String category, Integer emergencyRoom, String name) {
+    public Hospital(String id, String address, String category, Integer emergencyRoom, String name, String subdivision) {
         this.id = id;
         this.address = address;
-        this.district = district;
         this.category = category;
         this.emergencyRoom = emergencyRoom;
         this.name = name;
         this.subdivision = subdivision;
         this.setDistrict();
-        this.setSubdivision();
     }
 
     private void setDistrict() {
@@ -28,16 +26,6 @@ public class Hospital {
         this.district = district;
     }
 
-    private void setSubdivision() {
-        String[] subdivisions = {"의원", "병원", "한방병원",  "치과", "외과", "성형외과", "내과", "소아청소년과", "가정의학과", "피부과", "영상의학과", "정형외과", "흉부외과", "신경외과","산부인과", "재활의학과", "비뇨기과", "안과", "소아과", "정신건강의학과", "요양병원", "통증의학과", "이비인후과", "한의원"};
-        String subdivision = null;
-        for (String s : subdivisions) {
-            if (name.contains(s)) {
-                subdivision = s;
-            }
-        }
-        this.subdivision = subdivision;
-    }
 
     public String getId() {
         return id;
--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -3,15 +3,30 @@ package com.line.parser;
 import com.line.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital> {
+    //병원 이름을 받아서 세부과를 반환한다.
+    private String getSubdivision(String name) {
+        String[] subdivisions = {"의원", "병원", "한방병원",  "치과", "외과", "성형외과", "내과", "소아청소년과", "가정의학과", "피부과", "영상의학과", "정형외과", "흉부외과", "신경외과","산부인과", "재활의학과", "비뇨기과", "안과", "소아과", "정신건강의학과", "요양병원", "통증의학과", "이비인후과", "한의원"};
+        String subdivision = null;
+        for (String s : subdivisions) {
+            if (name.contains(s)) {
+                subdivision = s;
+            }
+        }
+        return subdivision;
+    }
+
     //csv파일에서 한 줄씩 받아서 객체로 만들어서 반환한다
     @Override
     public Hospital parse(String str) {
         //"(쌍따옴표)가 있는 파일이라면 아래 코드 사용, 현재 csv 파일에서는 없어서 사용 안 함
 //        str = str.replace("\"","");
+
         //csv 파일을 받기 때문에
         String[] splitted = str.split(",");
+        String subdivision = getSubdivision(splitted[10]);
+
         //id, address, category, emergencyRoom, name, subdivision
-        Hospital hospital = new Hospital(splitted[0], splitted[1], splitted[2], Integer.parseInt(splitted[6]), splitted[10]);
+        Hospital hospital = new Hospital(splitted[0], splitted[1], splitted[2], Integer.parseInt(splitted[6]), splitted[10], subdivision);
         return hospital;
     }
 }
