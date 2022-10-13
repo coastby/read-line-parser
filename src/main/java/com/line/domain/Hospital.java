ca317561e7192fee1826771813198c90cad16a9e
JO YEJI <whdpwl2@gmail.com>
2022-10-13 21:17:18 +0900

    병원 세부과 추가

--- src/main/java/com/line/domain/Hospital.java
+++ src/main/java/com/line/domain/Hospital.java
@@ -29,7 +29,7 @@ public class Hospital {
     }
 
     private void setSubdivision() {
-        String[] subdivisions = {"의원", "병원", "한방병원",  "치과", "성형외과", "내과", "소아청소년과", "가정의학과", "피부과", "영상의학과", "정형외과", "흉부외과", "신경외과","산부인과", "재활의학과", "비뇨기과", "안과", "소아과", "정신건강의학과", "요양병원", "통증의학과", "이비인후과", "한의원"};
+        String[] subdivisions = {"의원", "병원", "한방병원",  "치과", "외과", "성형외과", "내과", "소아청소년과", "가정의학과", "피부과", "영상의학과", "정형외과", "흉부외과", "신경외과","산부인과", "재활의학과", "비뇨기과", "안과", "소아과", "정신건강의학과", "요양병원", "통증의학과", "이비인후과", "한의원"};
         String subdivision = null;
         for (String s : subdivisions) {
             if (name.contains(s)) {
@@ -67,7 +67,7 @@ public class Hospital {
         return subdivision;
     }
     public String getAllForSql() {
-        String result = "('"+this.id+"', '"+this.address+"', '"+this.district+"', '"+this.category+"', '"+this.emergencyRoom+"', '"+this.name+"', '"+this.subdivision+"'),";
+        String result = "('"+this.id+"', '"+this.address+"', '"+this.district+"', '"+this.category+"', '"+this.emergencyRoom+"', '"+this.name+"', '"+this.subdivision+"'),\n";
         return result;
     }
 }
