e43322d52a48aecf27de8a360aa056e0d24fe021
JO YEJI <whdpwl2@gmail.com>
2022-10-18 13:27:24 +0900

    1. UserDao 생성 연습 2. HospitalDao : 1) add() : sql 파일이름을 넣으면 DB에 INSERT 2) findById() : get에서 이름 변경

--- src/main/java/com/line/dao/HospitalDao.java
+++ src/main/java/com/line/dao/HospitalDao.java
@@ -14,13 +14,14 @@ import java.util.List;
 
 public class HospitalDao {
     //csv filename 입력하면 sql로 저장
+    //첫 줄을 날려서 제목이 꼭 있어야 한다.
     public void add(String filename) throws SQLException, ClassNotFoundException, IOException {
         ConnectionMaker cm = new ConnectionMaker();
         LineReader lr = new LineReader(new HospitalParser());
         Connection conn = cm.makeConnection();
-
         PreparedStatement ps = conn.prepareStatement("INSERT INTO seoul_hospital(id, address, district, category, emergency_room, name, subdivision)"
                 +"VALUES (?, ?, ?, ?, ?, ?, ?);");
+
         List<Hospital> hospitals= lr.readLines(filename);
         for (Hospital hospital : hospitals) {
             ps.setString(1, hospital.getId());
@@ -30,11 +31,15 @@ public class HospitalDao {
             ps.setInt(5, hospital.getEmergencyRoom());
             ps.setString(6, hospital.getName());
             ps.setString(7, hospital.getSubdivision());
-
+            //쿼리를 저장했다가 한 번에 실행
+            ps.addBatch();
+            ps.clearParameters();
+            System.out.println(hospital.getId()+", "+hospital.getName()+"이 저장되었습니다.");
         }
+        ps.executeBatch();
+        System.out.println("데이터가 저장되었습니다.");
+        ps.clearBatch();
 
-
-        ps.executeUpdate();
         ps.close();
         conn.close();
     }
@@ -64,12 +69,10 @@ public class HospitalDao {
 
     }
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
         HospitalDao hd = new HospitalDao();
-        List<Hospital> hs = hd.findByName("삼성서울병원");
-        for (Hospital h : hs) {
-            System.out.println(h.getAddress());
-        }
+        String filename = "/Users/yj/Desktop/gitPrac/data/서울병원3개.csv";
+        hd.add(filename);
     }
 
 
