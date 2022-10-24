e32fa63b225a04825e11c5fe90746948b67ce047
JO YEJI <whdpwl2@gmail.com>
2022-10-24 14:20:12 +0900

    클래스 정리

--- src/main/java/com/dbexercise/dao/ConnectionMakerDeprecated.java
+++
@@ -1,9 +0,0 @@
-package com.dbexercise.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMakerDeprecated {
-    Connection makeConnection() throws SQLException;
-
-}
